5edd201fd01a06348fcc208df93d8e58d4fec13b
Corentin MACE <corentin.mace@ynov.com>
2018-11-27 16:28:13 +0100

    mdr v1

---
+++ assets/redir/navbar.php
@@ -0,0 +1,5 @@
+	<nav>
+			<a href="index.php">Accueil</a>
+			<a href="forum.php">Forum</a>
+			<a href="loginpage.php">Se connecter/Inscription</a>
+		</nav>
---
+++ loginpage.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+	<head>
+		<link rel="stylesheet" href="assets/style/style.css">
+		<meta charset="utf-8">
+		<title>Acceuil</title>
+	</head>
+	<body>
+	<header>
+		<?php include "assets/redir/navbar.php" ?>
+	</header>
+	</body>
+</html>
