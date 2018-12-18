6abb3a3f15a7ee104d997227b812929b369f72a5
Thomas.Godard <thomas.godard13@ynov.com>
2018-12-18 17:45:41 +0100

    MDP CHANGAIENT

--- membre.php
+++ membre.php
@@ -17,7 +17,7 @@
     </nav>
 
 
-<?php include("config-membre.php")²?>
+<?php include("config-membre.php")?>
 <?php if(isset($_GET['id']) AND $_GET['id'] > 0) {?>
    <body>
       <div class="phpclass" align="center">
@@ -41,5 +41,26 @@
           echo "Vous n'êtes pas connecté ! <a href=\"loginpage.php\">Se connecter</a>";
         }; ?>
       </div>
+			<p><input type="button" value="Changer mon mot de passe !">
+				<h1>Changer mon mot de passe</h1>
+       <?php
+      session_start();
+      if(isset($_SESSION['id']) AND isset($_SESSION['pseudo']))
+      {
+      ?>
+       <form class=chatform method="get" action="changer_mdp.php">
+      <p>
+      <label for="pseudo">Votre nouveau mot de passe :</label><input type="password" name="mdp" id="mdp" required/>
+      <br/>
+      <label for="pseudoConfirm">Retapez votre nouveau mot de passe :</label><input type="password" name="mdpConfirm" id="mdpConfirm" required/>
+      <br/>
+     <input type="submit" value="Changer mon mot de passe">
+     </form>
+     <?php
+     }
+     else{
+      echo '<p>Vous devez être connecté pour pouvoir changer votre mot de passe !</p>';
+     }
+     ?>
    </body>
 </html>
