4f34122c043a2a08ef95c29c7ff82e2d4e2223dd
QuentinNEP <tinoulegamer@gmail.com>
2018-12-18 14:45:19 +0100

    modif table.sql

---
+++ table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `post` (
+  `post_id` int(10) NOT NULL AUTO_INCREMENT,
+  `auteur` varchar(255) NOT NULL,
+  `texte` text NOT NULL,
+  PRIMARY KEY  (`post_id`),
+);
+
+CREATE TABLE `membres` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `pseudo` varchar(30) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+)
