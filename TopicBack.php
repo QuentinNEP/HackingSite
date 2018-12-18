aaff1681cba116609aba353d33a8cdbb6c215a5a
QuentinNEP <tinoulegamer@gmail.com>
2018-12-18 17:31:09 +0100

    TopicBack fini

--- TopicBack.php
+++ TopicBack.php
@@ -4,13 +4,41 @@ include("config-membre.php");
 
 $list_titre = $bdd->query('SELECT titre FROM post');
 $list_titre->fetch()['titre'];
-/*$auteur = $userinfo['pseudo'];*/
-$auteur = "tinou";
+$auteur = $userinfo['pseudo'];
   if (isset($_POST['submit'])){
-    $titre = htmlspecialchars($_POST['titre']);
-    $texte = htmlspecialchars($_POST['texte']);
     if(!empty($_POST['titre']) AND !empty($_POST['texte'])){
-      echo "okep";
+      $titre = htmlspecialchars($_POST['titre']);
+      $texte = htmlspecialchars($_POST['texte']);
+
+      $longueurTitre = strlen($titre);
+      if($longueurTitre >= 3){
+        if($longueurTitre <=30){
+          $longueurTexte =strlen($texte);
+
+          if($longueurTexte >= 5){
+            if($longueurTexte <= 800){
+              $reqtitre = $bdd->prepare("SELECT * FROM  WHERE titre = ?");
+              $reqtitre->execute(array($titre));
+              $titreExist = $reqtitre->rowCount();
+              if($titreExist == 0){
+                $inserttopic = $bdd->prepare("INSERT INTO post(auteur, texte, titre) VALUES(?, ?, ?)");
+                $inserttopic->execute(array($auteur, $texte, $titre));
+                echo "Votre topic a bien été créé ! <a href=\"forum.php\">Voir</a>";
+              }else{
+                echo "Un topic porte déja ce nom.";
+              }
+            }else{
+              echo "Texte trop long";
+            }
+          }else{
+            echo "Texte trop court";
+          }
+        }else{
+          echo "Titre trop long";
+        }
+      }else{
+        echo "Titre trop court";
+      }
     }else{
       echo "Veuillez remplir tout les champs";
     }
