e06a9faf1e7c595cd25c9319ac7d2dc96bbbfab1
QuentinNEP <tinoulegamer@gmail.com>
2018-12-18 17:14:58 +0100

    create topic debugging

--- TopicBack.php
+++ TopicBack.php
@@ -1,32 +1,27 @@
 <?php
 
-$bdd = new PDO('mysql:host=localhost;dbname=hacking_connexion;charset=utf8', 'root', '');
+include("config-membre.php");
 
 $list_titre = $bdd->query('SELECT titre FROM post');
 $list_titre->fetch()['titre'];
-$auteur = $userinfo['pseudo'];
-
-  if (isset($_POST['submit']))
-  {
-    $longueurTitre = strlen($_POST['titre']);
-    $longueurTexte = strlen($_POST['texte']);
-    if ($longueurTitre <= 1 || $longueurTitre >= 30){
-      if ($longueurTexte <= 10 || $longueurTexte >= 500){
-      	if(!empty($_POST['titre']) AND !empty($_POST['texte']))
-      	{
-          $titre = htmlspecialchars($_POST['titre']);
-          $texte = htmlspecialchars($_POST['texte']);
-
-          $reqtitre = $bdd->prepare("SELECT * FROM  WHERE titre = ?");
-          $reqtitre->execute(array($titre));
-          $titreExist = $reqtitre->rowCount();
-          if($titreExist > 0){
-            $inserttopic = $bdd->prepare("INSERT INTO post(auteur, texte, titre) VALUES(?, ?, ?)");
-            $inserttopic->execute(array($auteur, $texte, $titre));
-            echo "Votre topic a bien été créé ! <a href=\"forum.php\">Voir</a>";
-          }
-        }
-      }
+/*$auteur = $userinfo['pseudo'];*/
+$auteur = "tinou";
+  if (isset($_POST['submit'])){
+    $titre = htmlspecialchars($_POST['titre']);
+    $texte = htmlspecialchars($_POST['texte']);
+    if(!empty($_POST['titre']) AND !empty($_POST['texte'])){
+      echo "okep";
+    }else{
+      echo "Veuillez remplir tout les champs";
     }
+  }
+
 
 ?>
+<!-- $inserttopic = $bdd->prepare("INSERT INTO post(auteur, texte, titre) VALUES(?, ?, ?)");
+$inserttopic->execute(array($auteur, $texte, $titre));
+echo "Votre topic a bien été créé ! <a href=\"forum.php\">Voir</a>";
+
+$reqtitre = $bdd->prepare("SELECT * FROM  WHERE titre = ?");
+$reqtitre->execute(array($titre));
+$titreExist = $reqtitre->rowCount();-->
--- createTopic.php
+++ createTopic.php
@@ -2,9 +2,9 @@
 
 <form method="post" action="createTopic.php">
     <label for="titre">Titre du topic</label>
-        <input type="text" name="titre" id="titre" placeholder="Titre du topic" required>
+        <input type="text" name="titre" id="titre" placeholder="Titre du topic">
 
     <label for="texte">Texte</label>
-        <input type="text" name="texte" required>
+        <input type="text" name="texte"  >
             <button type="submit" name="submit"> Créer </button>
  </form>
