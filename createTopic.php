c6564189e5db2189ed9bb13bf33d3ad2d31ad3eb
QuentinNEP <tinoulegamer@gmail.com>
2019-01-08 15:42:54 +0100

    réparations

--- createTopic.php
+++ createTopic.php
@@ -1,10 +1,9 @@
-<?php
 
 
 <?php if(isset($getid) AND $getid > 0){  include("TopicBack.php");?>
 
 <form method="post" action="createTopic.php">
-    <label for="titre">Titre du topic</label>
+    <label for="titre">Titre du topic </label>
         <input type="text" name="titre" id="titre" placeholder="Titre du topic">
 
     <label for="texte">Texte</label>
--- membre.php
+++ membre.php
@@ -15,12 +15,11 @@
         <a href='registerpage.php'>S'inscrire</a>
         <a class="actif" href='membre.php'>Page membre</a>
     </nav>
+<?php include("assets\backPHP\config-membre.php") ?>
 
 
-<<<<<<< HEAD
-=======
 <?php if(isset($getid) AND $getid > 0) {?>
->>>>>>> 90813da6f2368ce23a22ee1dfe9ca9beba545d17
+
    <body>
       <div class="phpclass" align="center">
          <h2>Profil de <?php echo $userinfo['pseudo']; ?></h2>
@@ -30,38 +29,24 @@
          E-mail = <?php echo $userinfo['email']; ?>
          <br />
 
-         <?php
-         if(isset($_SESSION['id']) AND $userinfo['id'] == $_SESSION['id']) {
-         ?>
          <br />
          <a href="assets\backPHP\deconnect.php">Deconnect</a>
+				 <form class=chatform method="get" action="changer_mdp.php">
+				<p>
+				<label for="pseudo">Votre nouveau mot de passe :</label><input type="password" name="mdp" id="mdp" required/>
+				<br/>
+				<label for="pseudoConfirm">Retapez votre nouveau mot de passe :</label><input type="password" name="mdpConfirm" id="mdpConfirm" required/>
+				<br/>
+			 <input type="submit" value="Changer mon mot de passe">
+			 </form>
          <?php
          }
+				 else {
+					 echo "Vous devez être connecté pour accéder à ce profil.";
+				 }
          ?>
-       <?php }
-       else {
-          echo "Vous n'êtes pas connecté ! <a href=\"loginpage.php\">Se connecter</a>";
-        }; ?>
+
       </div>
-			<p><input type="button" value="Changer mon mot de passe !">
-				<h1>Changer mon mot de passe</h1>
-       <?php
-      if(isset($_SESSION['id']) AND isset($_SESSION['pseudo']))
-      {
-      ?>
-       <form class=chatform method="get" action="changer_mdp.php">
-      <p>
-      <label for="pseudo">Votre nouveau mot de passe :</label><input type="password" name="mdp" id="mdp" required/>
-      <br/>
-      <label for="pseudoConfirm">Retapez votre nouveau mot de passe :</label><input type="password" name="mdpConfirm" id="mdpConfirm" required/>
-      <br/>
-     <input type="submit" value="Changer mon mot de passe">
-     </form>
-     <?php
-     }
-     else{
-      echo '<p>Vous devez être connecté pour pouvoir changer votre mot de passe !</p>';
-     }
-     ?>
+
    </body>
 </html>
